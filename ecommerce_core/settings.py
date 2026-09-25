ff9fd2405d9ae49a6da7b90ae685496d4142bdca
Patnam Naga Sourya Deep <souryadeep940@gmail.com>
2026-09-25 18:57:45 +0530

    Update ALLOWED_HOSTS for Render

--- ecommerce_core/settings.py
+++ ecommerce_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6so*8g&x5uk3t@efv8gh^q+oo!99od4pd5njwbd6%nut0!g76o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-unnati.onrender.com', '.onrender.com', 'localhost', '127.0.0.1', '*']
 
 
 # Application definition
